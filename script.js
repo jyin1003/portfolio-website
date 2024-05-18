f3bae6843e98ccd8ea29ca5743eb38c46d656aed
jyin1003 <jyin1003@gmail.com>
2024-05-18 20:03:31 +1000

    Added a description box to the gallery picture popup.

--- script.js
+++ script.js
@@ -128,15 +128,14 @@ function filterSelection(c) {
     if (c == "all") c = "";
     // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
     for (i = 0; i < x.length; i++) {
-        w3RemoveClass(x[i], "show");
+        removeClass(x[i], "show");
         if (x[i].classList.contains(c) || c === "") {
-            w3AddClass(x[i], "show");
+            addClass(x[i], "show");
         }
     }
 }
-
 // Show filtered elements
-function w3AddClass(element, name) {
+function addClass(element, name) {
     var i, arr1, arr2;
     arr1 = element.className.split(" ");
     arr2 = name.split(" ");
@@ -146,9 +145,8 @@ function w3AddClass(element, name) {
         }
     }
 }
-
 // Hide elements that are not selected
-function w3RemoveClass(element, name) {
+function removeClass(element, name) {
     var i, arr1, arr2;
     arr1 = element.className.split(" ");
     arr2 = name.split(" ");
@@ -159,7 +157,6 @@ function w3RemoveClass(element, name) {
     }
     element.className = arr1.join(" ");
 }
-
 // Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("gallery-buttons");
 var btns = btnContainer.getElementsByClassName("btn");
@@ -171,43 +168,45 @@ for (var i = 0; i < btns.length; i++) {
     });
 }
 
-
+// POPUP ENLARGED GALLERY PICTURE
 // Get the popup container
 var popupContainer = document.getElementById("popup-container");
 var popupOverlay = document.querySelector(".popup-overlay");
-
 // Get all the popup links
 var popupLinks = document.querySelectorAll(".popup-picture");
-
 // Function to open the popup
 function openPopup(event) {
     event.preventDefault(); // Prevent default link behavior
     
     // Set the src attribute of the popup image
     var popupImage = document.getElementById("popup-image");
-    popupImage.src = this.href;
+    popupImage.src = this.getAttribute("href");
+
+    // Get description box for the popup
+    var descriptionContainer = document.getElementById("popup-description");
+    var description = this.getAttribute("data-description");
+    descriptionContainer.textContent = description;
     
     // Display the popup
-    popupContainer.style.display = "block";
+    popupContainer.style.display = "flex";
+    popupContainer.style.flexDirection = "column"; // Set flex-direction to column
+    popupContainer.style.alignItems = "center"; // Align vertically center
     popupOverlay.classList.add("popup-visible"); // Show overlay
     document.body.style.overflow = "hidden"; // Disable scrolling on the body
 }
-
 // Function to close the popup
 function closePopup() {
     popupContainer.style.display = "none";
     popupOverlay.classList.remove("popup-visible"); // Hide overlay
     document.body.style.overflow = "auto"; // Enable scrolling on the body
 }
-
 // Loop through each link and attach a click event listener
 popupLinks.forEach(function(link) {
     link.addEventListener("click", openPopup);
 });
-
 // Close the popup when the close button is clicked
 var closeBtn = document.querySelector("#popup-close");
 closeBtn.addEventListener("click", closePopup);
-
 // Close the popup when clicking outside of it
 popupOverlay.addEventListener("click", closePopup);
+
